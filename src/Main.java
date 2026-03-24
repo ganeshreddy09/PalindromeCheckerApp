44635b6a97393f7b59ab32ed63d5ef28dccccbfc
ganeshreddy09 <ganeshreddy87908@gmail.com>
2026-03-25 00:32:52 +0530

    UC11-PalindromeCheckerApp

--- src/Main.java
+++ src/Main.java
@@ -1,21 +1,50 @@
 import java.util.Scanner;
 
-class UC10_PalindromeCheckerApp {
+class PalindromeChecker {
+
+    private String word;
+
+    public PalindromeChecker(String word) {
+        this.word = word;
+    }
+
+    public String getWord() {
+        return word;
+    }
+
+    public void setWord(String word) {
+        this.word = word;
+    }
+
+    public boolean checkPalindrome() {
+        char[] chars = word.toLowerCase().replaceAll("[^a-z0-9]", "").toCharArray();
+        int start = 0;
+        int end = chars.length - 1;
+
+        while (start < end) {
+            if (chars[start] != chars[end]) {
+                return false;
+            }
+            start++;
+            end--;
+        }
+        return true;
+    }
+}
+
+class UC11_PalindromeCheckerApp {
 
     public static void main(String[] args) {
-        System.out.println("--- UC10: Case-Insensitive & Space-Ignored Palindrome ---");
+        System.out.println("--- UC11: Object-Oriented Palindrome Service ---");
 
         Scanner scanner = new Scanner(System.in);
         System.out.print("Input Text: ");
         String word = scanner.nextLine();
 
-        String normalized = word.toLowerCase().replaceAll("[^a-z0-9]", "");
-        String normalizedReversed = new StringBuilder(normalized).reverse().toString();
-        boolean isPalindrome = normalized.equals(normalizedReversed);
+        PalindromeChecker checker = new PalindromeChecker(word);
 
-        System.out.println("Input Text          : " + word);
-        System.out.println("Normalized Text     : " + normalized);
-        System.out.println("Is it a Palindrome? : " + isPalindrome);
+        System.out.println("Input Text          : " + checker.getWord());
+        System.out.println("Is it a Palindrome? : " + checker.checkPalindrome());
 
         scanner.close();
     }
