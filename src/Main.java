ac64d2850f932fe0001b78f3cf16a573d400c416
ganeshreddy09 <ganeshreddy87908@gmail.com>
2026-03-25 00:28:21 +0530

    UC9:Recursive Palindrome Checker

--- src/Main.java
+++ src/Main.java
@@ -1,72 +1,29 @@
 import java.util.Scanner;
 
-class UC8_PalindromeCheckerApp {
+class UC9_PalindromeCheckerApp {
 
-    static class Node {
-        char data;
-        Node next;
-
-        Node(char data) {
-            this.data = data;
-            this.next = null;
+    static boolean isPalindrome(String word, int start, int end) {
+        if (start >= end) {
+            return true;
         }
+        if (word.charAt(start) != word.charAt(end)) {
+            return false;
+        }
+        return isPalindrome(word, start + 1, end - 1);
     }
 
     public static void main(String[] args) {
-        System.out.println("--- UC8: Linked List Based Palindrome Checker ---");
+        System.out.println("--- UC9: Recursive Palindrome Checker ---");
 
         Scanner scanner = new Scanner(System.in);
         System.out.print("Input Text: ");
         String word = scanner.nextLine();
 
-        Node head = null;
-        Node tail = null;
-
-        for (int i = 0; i < word.length(); i++) {
-            Node newNode = new Node(word.charAt(i));
-            if (head == null) {
-                head = newNode;
-                tail = newNode;
-            } else {
-                tail.next = newNode;
-                tail = newNode;
-            }
-        }
-
-        Node slow = head;
-        Node fast = head;
-
-        while (fast != null && fast.next != null) {
-            slow = slow.next;
-            fast = fast.next.next;
-        }
-
-        Node prev = null;
-        Node current = slow;
-
-        while (current != null) {
-            Node nextNode = current.next;
-            current.next = prev;
-            prev = current;
-            current = nextNode;
-        }
-
-        Node left = head;
-        Node right = prev;
-        boolean isPalindrome = true;
-
-        while (right != null) {
-            if (left.data != right.data) {
-                isPalindrome = false;
-                break;
-            }
-            left = left.next;
-            right = right.next;
-        }
+        boolean result = isPalindrome(word, 0, word.length() - 1);
 
         System.out.println("Input Text          : " + word);
-        System.out.println("Is it a Palindrome? : " + isPalindrome);
+        System.out.println("Is it a Palindrome? : " + result);
 
         scanner.close();
     }
-}              }
+}
